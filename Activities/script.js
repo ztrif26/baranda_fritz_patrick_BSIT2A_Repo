d88a82dab92331eeca776aaa4b9bed7b08897515
Fritz <fbaranda@filamer.edu.ph>
2025-03-24 16:43:53 +0800

    Function Activity 1

---
+++ Activities/script.js
@@ -0,0 +1,14 @@
+function whatshallwear(temp) {
+    if (temp < 60){
+        console.log('wear a jacket')
+    }else if (temp < 70) {
+        console.log('wear a sweater')
+    }else {
+        console.log('wear a t-shirt')
+    }
+}
+
+
+whatshallwear(50);
+whatshallwear(80);
+whatshallwear(60);
